eb0f472fd5a01c965e42e213a03c6f32f542d1b3
Gudapureddy Rama Divya sree <106523077+Divyasree24@users.noreply.github.com>
2023-11-02 21:25:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function authenticate() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Implement authentication logic here, e.g., send a request to a backend server.
+}
+
+function submitFeedback() {
+    const feedback = document.getElementById('feedback').value;
+
+    // Implement feedback submission logic here, e.g., send feedback to a backend server.
+}
